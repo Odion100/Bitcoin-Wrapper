8b840006986baea4d46df97de64aa97ccf942df1
odion100 <odionedwards@gmail.com>
2021-11-20 06:58:28 -0500

    added nodemon and pm2

--- BC/index.js
+++ BC/index.js
@@ -3,9 +3,16 @@ const os = require("os");
 const bitcoind = require("bitcoind-rpc");
 const route = "rpc";
 const port = 5300;
-const host = "local";
+const host = "159.223.150.143";
 
-console.log(os.hostname());
-App.ServerModule("BC", function () {});
+console.log(os.hostname(), "test");
+App.ServerModule("BC", function () {
+  const BC = this;
+  BC.test = (data, cb) => {
+    console.log(data);
+    data.testPassed = true;
+    cb(null, data);
+  };
+});
 
-App.startService();
+App.startService({ route, port, host });
---
+++ app.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      script: "BC",
+      watch: true,
+      wait_ready: false,
+    },
+  ],
+};
